49c92870e91b84155b9ca11bdd5c625779aadb96
Karthikeyan <raghupathykartik@gmail.com>
2025-03-11 04:04:28 +0530

    Update signup form with database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import pg from 'pg';
 
 const app = express();
 const PORT = 8080;
@@ -8,6 +9,16 @@ let posts = [];
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "projects",
+  password: "Savitha@12",
+  port: 5432,
+});
+
+db.connect();
 
 app.get('/', (req, res) => {
   res.sendFile('index.html');
@@ -33,9 +44,17 @@ app.get('/home', (req, res) => {
 app.get('/login', (req, res) => {
   res.render('partials/login.ejs')
 });
+
 app.get('/sign-up', (req, res) => {
   res.render('partials/signup.ejs')
-})
+});
+
+app.post('/signup', async(req, res) => {
+  const {phone, name, email, password} = req.body;
+  console.log(phone, name, password, email);
+  await db.query("INSERT INTO cartcane (phone, name, email, password) VALUES ($1, $2, $3, $4)", [phone, name, email, password]);
+  res.redirect("/login");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
