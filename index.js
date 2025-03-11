b138f1efe24113bbfc90788fa233e33ad9a76f17
Karthikeyan <raghupathykartik@gmail.com>
2025-03-11 18:13:00 +0530

    Update database for form query, added accept button and remove temporary storage.

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import pg from 'pg';
 
 const app = express();
 const PORT = 8080;
-let posts = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -29,15 +28,20 @@ app.post('/info-form', (req, res) => {
   res.render("partials/initialform.ejs", {subject: data});
 });
 
-app.post("/home", (req, res) => {
+app.post("/home", async(req, res) => {
   const {subject, deadline, price, contact, description} = req.body;
-  const content = {subject, deadline, price, contact, description};
-  posts.unshift(content);
+  try{
+    db.query("INSERT INTO user_data (subject, deadline, payment, phone, description) VALUES ($1, $2, $3, $4, $5)",[subject, deadline, price, contact, description]);
+    console.log("success");
+  } catch (err) {
+    console.log("Something wrong with database:", err);
+  };
   res.redirect("/home");
-  // res.render("partials/home.ejs", {post: posts});
 })
 
-app.get('/home', (req, res) => {
+app.get('/home', async(req, res) => {
+  const data = await db.query("SELECT * FROM user_data ORDER BY id DESC");
+  let posts = data.rows;
   res.render('partials/home.ejs', {post: posts});
 });
 
@@ -52,7 +56,7 @@ app.get('/sign-up', (req, res) => {
 app.post('/signup', async(req, res) => {
   const {phone, name, email, password} = req.body;
   console.log(phone, name, password, email);
-  await db.query("INSERT INTO cartcane (phone, name, email, password) VALUES ($1, $2, $3, $4)", [phone, name, email, password]);
+  await db.query("INSERT INTO clients_data (phone, name, email, password) VALUES ($1, $2, $3, $4)", [phone, name, email, password]);
   res.redirect("/login");
 });
 
