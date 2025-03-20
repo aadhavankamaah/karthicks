ae89baa45d3484b000c35c6259ea1bc2649ba4bb
Karthikeyan <raghupathykartik@gmail.com>
2025-03-21 01:21:19 +0530

    modify user profile

--- public/form.js
+++ public/form.js
@@ -1,1 +1,6 @@
+const profile = document.querySelector(".user-profile");
+const userDropdown = document.getElementById("userDropdown");
+profile.addEventListener("click", ()=>{
+    userDropdown.classList.toggle("hide");
+});
 
